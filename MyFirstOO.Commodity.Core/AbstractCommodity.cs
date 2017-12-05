b47058c7e43964f941e1a4f31a223d7e1a58ff74
wangyongmao <wangyongmao@yiguo.com>
2017-12-05 22:50:20 +0800

    [init]源代码仓库

---
+++ MyFirstOO.Commodity.Core/AbstractCommodity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstOO.Commodity.Core
+{
+    /// <summary>
+    /// 商品抽象类，商品的最底层溯源对象
+    /// </summary>
+    public abstract class AbstractCommodity
+    {
+        
+    }
+}
---
+++ MyFirstOO.Commodity.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyFirstOO.Commodity.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyFirstOO.Commodity.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1e96a005-9e45-4b6b-b5b9-01c3ad309180")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
